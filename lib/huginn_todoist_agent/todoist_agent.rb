a3c8446f7137f60e413ec66ed3b444ae9f27109e
Tyler Guillen <tyguillen@gmail.com>
2018-05-06 22:42:47 -0700

    labels can be passed as strings for label names

--- lib/huginn_todoist_agent/todoist_agent.rb
+++ lib/huginn_todoist_agent/todoist_agent.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Agents
   class TodoistAgent < Agent
     include FormConfigurable
@@ -25,7 +27,7 @@ module Agents
 
         `project_id` is the *ID* of the project to add the item to. Leave blank for INBOX.
 
-        `labels` is a comma-seperated list of label IDs (or blank for no labels).
+        `labels` is a comma-seperated list of label names (or blank for no labels).
 
         `prority` is an integer from 1 to 4.  Where 1 means lowest (natural) priority and
         4 highest.  Defaults to natural priority (aka 1).
@@ -34,12 +36,12 @@ module Agents
 
     def default_options
       {
-        "api_token" => "",
-        "content" => "{{ content }}",
-        "date_string" => "today",
-        "project_id" => "",
-        "labels" => "",
-        "priority" => "",
+        'api_token' => '',
+        'content' => '{{ content }}',
+        'date_string' => 'today',
+        'project_id' => '',
+        'labels' => '',
+        'priority' => ''
       }
     end
 
@@ -55,29 +57,49 @@ module Agents
     end
 
     def validate_options
-      errors.add(:base, "you need to specify your Todoist API token or provide a credential named todoist_api_token") unless options["api_token"].present? || credential("todoist_api_token").present?
+      errors.add(:base, 'you need to specify your Todoist API token or provide a credential named todoist_api_token') unless options['api_token'].present? || credential('todoist_api_token').present?
     end
 
     def receive(incoming_events)
-      incoming_events.each do |event|
-        interpolate_with(event) do
-          item = { "content" => interpolated["content"] }
-          item["date_string"] = interpolated["date_string"] if interpolated["date_string"].present?
-          item["project_id"] = interpolated["project_id"].to_i if interpolated["project_id"].present?
-          item["priority"] = interpolated["priority"].to_i if interpolated["priority"].present?
+      incoming_events.each { |event| handle_event(event) }
+    end
 
-          if interpolated["labels"].present?
-            item["labels"] = interpolated["labels"].split(%r{,\s*}).map(&:to_i)
-          end
+    def handle_event(event)
+      interpolate_with(event) do
+        item = { 'content' => interpolated['content'] }
+        item['date_string'] = interpolated['date_string'] if interpolated['date_string'].present?
+        item['project_id'] = interpolated['project_id'].to_i if interpolated['project_id'].present?
+        item['priority'] = interpolated['priority'].to_i if interpolated['priority'].present?
 
-          log "creating item: #{item}"
-          todoist = Todoist::Client.create_client_by_token(interpolated["api_token"].present? ? interpolated["api_token"] : credential("todoist_api_token"))
-          todoist.sync_items.add(item)
-          todoist.sync
-        end
+        labels = label_ids_for(interpolated['labels'])
+        item['labels'] = labels if labels.present?
+
+        add_item(item)
       end
     end
 
+    def todoist
+      @todoist ||= Todoist::Client.create_client_by_token(
+        interpolated['api_token'].present? ? interpolated['api_token'] : credential('todoist_api_token')
+      )
+    end
+
+    def add_item(item_params)
+      todoist.sync_items.add(item_params)
+      todoist.sync
+    end
+
+    def fetch_labels
+      todoist.sync_labels.collection.values
+    end
+
+    def label_ids_for(label_list)
+      label_names = label_list.split(/,\s*/)
+      label_names.map { |label_name| label_id_for(label_name) }.compact
+    end
+
+    def label_id_for(label_name)
+      fetch_labels.find { |label| label.name == label_name }.try(:id)
+    end
   end
 end
-
